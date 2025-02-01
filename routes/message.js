3bb36e170ce156b53b59f83382a0dd2048f4f99a
Ayuk giress <giress865@gmail.com>
2025-02-01 11:24:46 +0100

    intailizing chart withen app

--- routes/message.js
+++ routes/message.js
@@ -1,14 +1,11 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-import { getUserforSidebar } from "../controllers/message.controllers.js";
-import { getmessages } from "../controllers/message.controllers.js";
-import { sendMessage } from "../controllers/message.controllers.js";
+import { getUserforSidebar, getmessages, sendMessage } from "../controllers/message.controllers.js";
 
-const router = express.Router;
+const router = express.Router();  
 
 router.get("/users", auth, getUserforSidebar);
-
 router.get("/:id", auth, getmessages);
-
 router.post("/send/:id", auth, sendMessage);
+
 export default router;
