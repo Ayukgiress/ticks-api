fc3d374a998d9448cef40b9df5520ef07a452304
Ayuk <giress865@gmail.com>
2025-11-11 09:15:13 +0100

    Fix Google OAuth callback URL to handle trailing slashes

--- passport-setup.js
+++ passport-setup.js
@@ -14,7 +14,7 @@ const initializePassport = (app) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `${process.env.BACKEND_URL}/users/google/callback`,
+        callbackURL: `${process.env.BACKEND_URL?.replace(/\/+$/, '')}/users/google/callback`,
         scope: ['profile', 'email'],
       },
       async (accessToken, refreshToken, profile, done) => {
